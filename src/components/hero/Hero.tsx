0957a7912b00583d07d57501113fce5f1d850760
iii <miselrkba@gmail.com>
2021-06-13 09:12:25 +0200

    feat: added swiper slider base

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -1,11 +1,69 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
 import './Hero.scss';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import SwiperCore, { Autoplay, Pagination, Navigation } from 'swiper/core';
+import 'swiper/swiper-bundle.css';
+SwiperCore.use([Autoplay, Pagination, Navigation]);
 
 const Hero = () => {
   return (
-    <div>
-      <h1>Hero</h1>
-    </div>
+    <section className="hero">
+      <Swiper
+        spaceBetween={30}
+        centeredSlides={true}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <h1>Lorem, ipsum dolor.</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam,
+            officiis. Consectetur quos incidunt nam dolor in officia
+            voluptatibus delectus nemo.
+          </p>
+          <button>See More</button>
+          <button>Get More</button>
+        </SwiperSlide>
+        <SwiperSlide>
+          <h1>Lorem, ipsum dolor.</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam,
+            officiis. Consectetur quos incidunt nam dolor in officia
+            voluptatibus delectus nemo.
+          </p>
+          <button>See More</button>
+          <button>Get More</button>
+        </SwiperSlide>
+        <SwiperSlide>
+          <h1>Lorem, ipsum dolor.</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam,
+            officiis. Consectetur quos incidunt nam dolor in officia
+            voluptatibus delectus nemo.
+          </p>
+          <button>See More</button>
+          <button>Get More</button>
+        </SwiperSlide>
+        <SwiperSlide>
+          <h1>Lorem, ipsum dolor.</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam,
+            officiis. Consectetur quos incidunt nam dolor in officia
+            voluptatibus delectus nemo.
+          </p>
+          <button>See More</button>
+          <button>Get More</button>
+        </SwiperSlide>
+      </Swiper>
+    </section>
   );
 };
 
